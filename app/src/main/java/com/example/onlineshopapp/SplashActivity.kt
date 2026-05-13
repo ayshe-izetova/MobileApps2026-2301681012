b7e400145a6af33120eb8f44401b767b58d2960b
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2026-05-13 21:26:17 +0300

    Add splash screen activity

--- app/src/main/java/com/example/onlineshopapp/SplashActivity.kt
+++ app/src/main/java/com/example/onlineshopapp/SplashActivity.kt
@@ -3,18 +3,24 @@ package com.example.onlineshopapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.onlineshopapp.databinding.ActivitySplashBinding
+import android.content.Intent
+
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
+
+
+
     }
 }
