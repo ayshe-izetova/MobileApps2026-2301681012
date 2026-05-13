ca34ba31c6d4b236fb727fbb3016932a810c52bc
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2026-05-13 21:57:11 +0300

    Fix splash activity ktlint formatting

--- app/src/main/java/com/example/onlineshopapp/SplashActivity.kt
+++ app/src/main/java/com/example/onlineshopapp/SplashActivity.kt
@@ -1,26 +1,23 @@
 package com.example.onlineshopapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.onlineshopapp.databinding.ActivitySplashBinding
-import android.content.Intent
-
 
 class SplashActivity : AppCompatActivity() {
-    lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivitySplashBinding.inflate(layoutInflater)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.startBtn.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
-
-
-
     }
 }
