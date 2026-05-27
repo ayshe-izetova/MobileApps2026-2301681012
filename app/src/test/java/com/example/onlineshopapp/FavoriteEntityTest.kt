8f09801d666cf7985c81060a575e049fb97819b7
Ayshe Izetova <stu2301681012@uni-plovdiv.bg>
2026-05-27 22:12:36 +0300

    Add unit and UI tests

---
+++ app/src/test/java/com/example/onlineshopapp/FavoriteEntityTest.kt
@@ -0,0 +1,31 @@
+package com.example.onlineshopapp
+
+import com.example.onlineshopapp.data.local.FavoriteEntity
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FavoriteEntityTest {
+    @Test
+    fun favoritePrice_updateSuccessfully() {
+        val favorite = FavoriteEntity(
+            id = 1,
+            title = "Apple Watch",
+            price = 150.0,
+            imageUrl = "",
+        )
+
+        assertEquals(150.0, favorite.price, 0.0)
+    }
+
+    @Test
+    fun favoriteTitle_isCorrect() {
+        val favorite = FavoriteEntity(
+            id = 2,
+            title = "Samsung Watch",
+            price = 200.0,
+            imageUrl = "",
+        )
+
+        assertEquals("Samsung Watch", favorite.title)
+    }
+}
